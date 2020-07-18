cdd1aacdd146e08eb384895e8b955b79580e5bbc
saurabh98s <geek.saurabh98@gmail.com>
2020-07-19 02:46:58 +0530

    removing some files

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -15,7 +15,7 @@ func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) 
 	todo := &model.Todo{
 		Text:   input.Text,
 		ID:     fmt.Sprintf("T%d", rand.Int()),
-		UserID: input.UserID, // fix this line
+		// UserID: input.UserID, // fix this line
 	}
 	r.todos = append(r.todos, todo)
 	return todo, nil
@@ -37,16 +37,3 @@ func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *todoResolver) User(ctx context.Context, obj *model.Todo) (*model.User, error) {
-	return &model.User{ID: obj.UserID, Name: "user " + obj.UserID}, nil
-}
-func (r *Resolver) Todo() generated.TodoResolver { return &todoResolver{r} }
-
-type todoResolver struct{ *Resolver }
